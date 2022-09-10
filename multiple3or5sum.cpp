4e2765af0cbe02560a61ad84de35e145f90faca5
Kanika Garg <reachkanikagarg@gmail.com>
2022-09-11 00:40:43 +0530

    Program to find sum of all the mulitples of 3 or 5 till 1000

---
+++ multiple3or5sum.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file multiple3or5sum.cpp
+ * @author Kanika Garg
+ * @brief 
+ * @version 0.1
+ * @date 2022-09-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ * Problem statement
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+#include <iostream>
+int main(){
+    const int three = 3;
+    const int five = 5;
+    int sumOfMultiples = 0;
+    for(int i=3;i<1000;i++){
+        if (i%three == 0  or i%five ==0){
+            sumOfMultiples+=i;
+        }
+    }
+    std::cout<<sumOfMultiples<<std::endl;
+}
