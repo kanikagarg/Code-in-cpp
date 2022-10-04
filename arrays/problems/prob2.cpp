d6672b8019c00809009e66d870ac5fee26b616a6
Kanika Garg <reachkanikagarg@gmail.com>
2022-10-04 13:31:16 +0530

    Sort an array of 0s, 1s and 2s

---
+++ arrays/problems/prob2.cpp
@@ -0,0 +1,88 @@
+/**
+ * @file prob2.cpp
+ * @author Kanika Garg (reachkanikagarg@gmail.com)
+ * @brief Sort an array of 0s, 1s and 2s
+ * @version 0.1
+ * @date 2022-10-04
+ * 
+ * @copyright Copyright (c) 2022
+ * @details Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+ * Example 1:
+ * Input:  N = 5 arr[]= {0 2 1 2 0}
+ * Output: 0 0 1 2 2
+ * Explanation: 0s 1s and 2s are segregated into ascending order.
+ * Example 2: Input:  N = 3  arr[] = {0 1 0}
+ *            Output:0 0 1
+ *            Explanation: 0s 1s and 2s are segregated into ascending order.
+ * Your Task: You don't need to read input or print anything. Your task is to complete the function sort012() that takes an array arr and N as input parameters and sorts the array in-place.
+ * Expected Time Complexity: O(N)
+ * Expected Auxiliary Space: O(1)
+ * Constraints: 1 <= N <= 10^6
+ * 0 <= A[i] <= 2
+ 
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        int i=0, j=n,zero=0,one=0,two=0;
+        for(i=0;i<j;i++){
+            if (a[i]==0){
+                zero++;
+            }else if(a[i]==1){
+                one++;
+            }else if(a[i]==2){
+                two++;
+            }
+        }
+        
+        for( i=0;i<j;i++){
+            if(zero>0){
+                a[i]=0;
+                zero--;
+            }else if(one>0){
+                a[i]=1;
+                one--;
+            }else if(two>0){
+                a[i]=2;
+                two--;
+            }    
+        }
+
+    }
+    
+};
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
+
