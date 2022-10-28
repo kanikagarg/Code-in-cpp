97ede293a5abc634414f9e40bcd75fe8ccbf03d4
Kanika Garg <reachkanikagarg@gmail.com>
2022-10-28 16:06:46 +0530

    level with maximum sum

--- trees/traversal/levelorder/max_levelsum.cpp
+++ trees/traversal/levelorder/max_levelsum.cpp
@@ -1,6 +1,6 @@
 /**
  * @file max_levelsum.cpp
- * @author your name (you@domain.com)
+ * @author Kanika Garg (reachkanikagarg@gmail.com)
  * @brief Maximum Level Sum of a Binary Tree
  * Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
  * Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
@@ -12,21 +12,15 @@
  * Level 2 sum = 7 + 0 = 7.
  * Level 3 sum = 7 + -8 = -1.
  * So we return the level with the maximum sum which is level 2.
- * 
  * Example 2:
  * Input: root = [989,null,10250,98693,-89388,null,null,null,-32127]
  * Output: 2
- * 
  * Constraints:
  * The number of nodes in the tree is in the range [1, 104].
  * -105 <= Node.val <= 105
- * 
- * 
  * @version 0.1
  * @date 2022-10-03
- * 
  * @copyright Copyright (c) 2022
- * 
  */
 
 /**
@@ -40,6 +34,19 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#include<iostream>
+using namespace std;
+#include <vector>
+#include<queue>
+
+ struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
 
 
 class Solution {
